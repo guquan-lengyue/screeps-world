4112faf51fbe28c5d1023aac80922561bb52b4ee
孤泉冷月 <guquanlengyue@gmail.com>
2022-06-08 23:07:28 +0800

    tutorial-1 pass

--- loop.cpp
+++ loop.cpp
@@ -4,6 +4,8 @@
 #include <Screeps/RoomObject.hpp>
 #include <Screeps/RoomPosition.hpp>
 #include <Screeps/Source.hpp>
+#include <Screeps/Store.hpp>
+#include <Screeps/StructureSpawn.hpp>
 #include <Screeps/Constants.hpp>
 #include <emscripten.h>
 #include <emscripten/bind.h>
@@ -13,18 +15,30 @@ extern "C" void loop()
 {
 	Screeps::Context::update();
 
-	auto creeps = Screeps::Game.creeps();
-	auto creeps_it = creeps.find("Harvester1");
-	auto creep = (*creeps_it).second;
-	creep.say("hello world");
-	Screeps::Room room = creep.room();
+	std::map<std::string, Screeps::Creep> creeps = Screeps::Game.creeps();
+	for (auto &c : creeps)
+	{
+		Screeps::Creep creep = c.second;
+		Screeps::StructureSpawn spawn1 = Screeps::Game.spawns().find("Spawn1")->second;
+		Screeps::Room room = creep.room();
 
-	std::vector<std::unique_ptr<Screeps::RoomObject>> sources = room.find(Screeps::FIND_SOURCES);
-	// Screeps::Source source(sources[0].get()->value());
-	// if (creep.harvest(source) == Screeps::ERR_NOT_IN_RANGE)
-	// {
-	// 	creep.moveTo(source);
-	// }
+		if (creep.store().getFreeCapacity() > 0)
+		{
+			std::vector<std::unique_ptr<Screeps::RoomObject>> sources = room.find(Screeps::FIND_SOURCES);
+			Screeps::Source source(sources[0].get()->value());
+			if (creep.harvest(source) == Screeps::ERR_NOT_IN_RANGE)
+			{
+				creep.moveTo(source);
+			}
+		}
+		else
+		{
+			if (creep.transfer(spawn1, Screeps::RESOURCE_ENERGY) == Screeps::ERR_NOT_IN_RANGE)
+			{
+				creep.moveTo(spawn1);
+			}
+		}
+	}
 }
 
 EMSCRIPTEN_BINDINGS(loop)

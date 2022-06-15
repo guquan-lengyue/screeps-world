e0a7c6d2ebd5cfa1031af751cc5233bcdbb72c13
孤泉冷月 <guquanlengyue@gmail.com>
2022-06-15 18:58:26 +0800

    fix

--- loop.cpp
+++ loop.cpp
@@ -23,7 +23,7 @@ extern "C" void loop() {
 
     std::map<std::string, Screeps::Creep> creeps = Screeps::Game.creeps();
 
-    Screeps::StructureSpawn homeSpawn = Screeps::Game.spawns().find("home")->second;
+    Screeps::StructureSpawn homeSpawn = Screeps::Game.spawns().find("孤泉冷月")->second;
     auto sources = homeSpawn.room().find(Screeps::FIND_SOURCES);
 
 
@@ -55,7 +55,7 @@ extern "C" void loop() {
         std::unique_ptr<Screeps::Structure> s(new Screeps::Structure(structures.begin()->get()->value()));
         container = std::move(s);
     }
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 3; i++) {
         homeSpawn.spawnCreep(Builder::bodyParts(), Builder::namePre() + std::to_string(i));
     }
     for (int i = 0; i < 2; i++) {

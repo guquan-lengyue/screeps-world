bf9d8c06b512129e9c506d6deaa33a9c7d05a3b0
孤泉冷月 <guquanlengyue@gmail.com>
2022-07-15 15:16:16 +0800

    deploy

--- system/component/object/Spawn.hpp
+++ system/component/object/Spawn.hpp
@@ -1,18 +1,32 @@
 #ifndef OBJECT_SPAWN_HPP
 #define OBJECT_SPAWN_HPP
-
 #include <Screeps/StructureSpawn.hpp>
 #include <Screeps/JSON.hpp>
 #include <string>
-#include <utility>
-#include "BaseInterface.hpp"
-
-class Spawn : public Screeps::StructureSpawn, IMemory {
+class Spawn : public Screeps::StructureSpawn
+{
 public:
-    using IMemory::setMemory;
-    using IMemory::getMemory;
     Spawn(JS::Value creep);
+    std::string getMemory(std::string key);
+    bool setMemory(std::string key, std::string value);
 };
 
-Spawn::Spawn(JS::Value creep) : Screeps::StructureSpawn(std::move(creep)) {};
+Spawn::Spawn(JS::Value creep) : Screeps::StructureSpawn(creep){};
+
+std::string Spawn::getMemory(std::string key)
+{
+    JSON memory = this->memory();
+    std::string value;
+    memory[key].get_to(value);
+    return value;
+}
+bool Spawn::setMemory(std::string key, std::string value)
+{
+    JSON memory = this->memory();
+    memory[key] = value;
+    Screeps::StructureSpawn::setMemory(memory);
+    return true;
+}
+
+
 #endif
